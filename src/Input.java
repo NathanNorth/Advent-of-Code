6d51d691fd5d8389f6947a01e89c406877cdce0f
NathanNorth <51377975+NathanNorth@users.noreply.github.com>
2021-12-01 10:19:17 -0500

    privatize cookies

---
+++ src/Input.java
@@ -0,0 +1,72 @@
+import java.io.File;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Input {
+    public static List<String> getInputLinesWeb(int day) {
+        System.out.println("Attempting to get input lines...");
+
+        //make local copies
+        File exists = new File("./inputs/day" + day + ".txt");
+        if (exists.exists()) {
+            System.out.println("Using local copy!");
+            return getInputLinesLocal(day);
+        }
+
+        System.out.println("Grabbing online copy!");
+
+        try {
+            HttpURLConnection con = (HttpURLConnection) new URL("https://adventofcode.com/2021/day/1/input").openConnection();
+            con.setRequestMethod("GET");
+            con.addRequestProperty("Cookie", "session=" + getKeys().get(0));
+            Scanner s = new Scanner(con.getInputStream());
+            String cache = "";
+            List<String> returnable = new ArrayList<>();
+            while (s.hasNextLine()) {
+                String line = s.nextLine();
+
+                returnable.add(line);
+                cache += line + "\n";
+            }
+            Files.writeString(Path.of("./inputs/day" + day + ".txt"), cache);
+            return returnable;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    private static List<String> getInputLinesLocal(int day) {
+        try {
+            return Files.readAllLines(Path.of("./inputs/day" + day + ".txt"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    //keys.txt is stored in root dir and holds instance-specific data (eg. bot token)
+    private static List<String> keys = null;
+
+    public static List<String> getKeys() {
+        if (keys == null) {
+            try {
+                keys = Files.readAllLines(Paths.get("./keys.txt"));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            //filter out things we commented out in our keys
+            for (int i = keys.size() - 1; i >= 0; i--) {
+                if (keys.get(i).indexOf('#') == 0) keys.remove(i);
+            }
+        }
+        return keys;
+    }
+}
