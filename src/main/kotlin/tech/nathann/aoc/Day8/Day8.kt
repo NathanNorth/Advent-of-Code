39087dd49823e8395b44a10574557e6bb338ce46
NathanNorth <51377975+NathanNorth@users.noreply.github.com>
2021-12-08 05:53:27 -0500

    day 8 start

---
+++ src/main/kotlin/tech/nathann/aoc/Day8/Day8.kt
@@ -0,0 +1,58 @@
+package tech.nathann.aoc.Day8
+
+import tech.nathann.aoc.Input
+
+val inputData = Input.getInputLinesWeb(8)
+
+fun main() {
+    val s = "gdshlf".toCharArray()
+
+
+    println(doTheThing())
+    part2()
+}
+fun doTheThing(): Int {
+    val flatList = inputData.toList().map { it.split("|")[1].split(" ") }.flatten()
+    return flatList.count {
+        it.length == 2 || it.length == 3 || it.length == 4 || it.length == 7
+    }
+}
+fun part2() {
+    val data = inputData.map { it.split("|")[0].split(" ")
+        .map {
+            val arr = it.toCharArray()
+            //arr.sort()
+            String(arr)
+        }
+    }
+    val output = inputData.map { it.split("|")[1].split(" ")
+        .map {
+            val arr = it.toCharArray()
+            //arr.sort()
+            String(arr)
+        }
+    }
+
+    data.zip(output).map { (data, output) -> Line(data, output) }
+        .map { println(it.map) }
+}
+data class Line(val data: List<String>, val output: List<String>) {
+    val map = HashMap<Int, String>()
+    init {
+        println(data)
+        println(output)
+        for(s in output) {
+            println(s)
+            val num = when(s.length)
+            {
+                2 -> 1
+                3 -> 7
+                4 -> 4
+                7 -> 8
+                else -> -1
+            }
+            if(num != -1)
+                map[num] = s
+        }
+    }
+}
